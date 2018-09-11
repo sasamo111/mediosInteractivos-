47d2f551dca38b2bee07d5d4e733b4e81e7d6145
sasamo111 <42587752+sasamo111@users.noreply.github.com>
2018-09-11 14:35:13 -0500

    Update sketch.js

--- 04/sketch.js
+++ 04/sketch.js
@@ -87,7 +87,7 @@ function setup() {
   background(fond);
 
   fill(250, 244, 227);
-  rect(20, 20, 650, 170, )
+  rect(20, 20, 650, 170);
 
   //Botones visibles
 
@@ -123,118 +123,182 @@ function setup() {
   fill(255);
   rect(posXbot20, posYbot20, tamBot20, tamBot20);
   fill(255);
-
+  
+    
 }
 
 function draw() {
+  //Icono del helado para el boton
 
-  if (mouseX > posXbot1 && mouseX < posXbot1 + tamBot1 &&
-    mouseY > posYbot1 && mouseY < posYbot1 + tamBot1) {
-    herSel = 0
-  }
+  ellipse(75, 65, 25, 25);
+  triangle(60, 65, 90, 65, 75, 98);
 
-  if (mouseX > posXbot2 && mouseX < posXbot2 + tamBot2 &&
-    mouseY > posYbot2 && mouseY < posYbot2 + tamBot2) {
-    herSel = 1
-  }
+  //Icono de la flecha para el boton
 
-  if (mouseX > posXbot3 && mouseX < posXbot3 + tamBot3 &&
-    mouseY > posYbot3 && mouseY < posYbot3 + tamBot3) {
-    herSel = 2
-  }
+  line(115, 55, 155, 95);
+  line(115, 55, 155, 55);
+  line(115, 55, 115, 95);
 
-  if (mouseX > posXbot4 && mouseX < posXbot4 + tamBot4 &&
-    mouseY > posYbot4 && mouseY < posYbot4 + tamBot4) {
-    herSel = 3
-  }
+  //Icono reloj de arena para boton
 
-  if (mouseX > posXbot5 && mouseX < posXbot5 + tamBot5 &&
-    mouseY > posYbot5 && mouseY < posYbot5 + tamBot5) {
-    herSel = 4
-  }
+  quad(175, 55, 215, 55, 175, 95, 215, 95);
 
-  if (mouseX > posXbot6 && mouseX < posXbot6 + tamBot6 &&
-    mouseY > posYbot6 && mouseY < posYbot6 + tamBot6) {
-    herSel = 5
-  }
+  //Icono X
 
-  if (mouseX > posXbot7 && mouseX < posXbot7 + tamBot7 &&
-    mouseY > posYbot7 && mouseY < posYbot7 + tamBot7) {
-    herSel = 6
-  }
+  line(235, 55, 275, 95);
+  line(275, 55, 235, 95);
 
-  if (mouseX > posXbot8 && mouseX < posXbot8 + tamBot8 &&
-    mouseY > posYbot8 && mouseY < posYbot8 + tamBot8) {
-    herSel = 7
-  }
+  //Icono estrella
 
-  if (mouseX > posXbot9 && mouseX < posXbot9 + tamBot9 &&
-    mouseY > posYbot9 && mouseY < posYbot9 + tamBot9) {
-    herSel = 8
-  }
+  ellipse(315, 75, 15, 15);
+  triangle(310, 70, 295, 75, 310, 80);
+  triangle(310, 70, 315, 55, 321, 70);
+  triangle(321, 70, 335, 75, 321, 80);
+  triangle(321, 80, 315, 95, 310, 80);
 
-  if (mouseX > posXbot10 && mouseX < posXbot10 + tamBot10 &&
-    mouseY > posYbot10 && mouseY < posYbot10 + tamBot10) {
-    herSel = 9
-  }
+  // Sol
 
-  if (mouseX > posXbot11 && mouseX < posXbot11 + tamBot11 &&
-    mouseY > posYbot11 && mouseY < posYbot11 + tamBot11) {
-    colSel = 0
-  }
+  line(375, 75, 375, 100);
+  line(375, 75, 375, 50);
+  line(375, 75, 400, 75);
+  line(375, 75, 350, 75);
+  line(375, 75, 400, 100);
+  line(375, 75, 350, 50);
+  line(375, 75, 400, 50);
+  line(375, 75, 350, 100);
+  ellipse(375, 75, 25, 25);
 
-  if (mouseX > posXbot12 && mouseX < posXbot12 + tamBot12 &&
-    mouseY > posYbot12 && mouseY < posYbot12 + tamBot12) {
-    colSel = 1
-  }
+  // Cruz con ellipses
 
-  if (mouseX > posXbot13 && mouseX < posXbot13 + tamBot13 &&
-    mouseY > posYbot13 && mouseY < posYbot13 + tamBot13) {
-    colSel = 2
-  }
+  ellipse(435, 75, 10, 40);
+  ellipse(435, 75, 40, 10);
 
-  if (mouseX > posXbot14 && mouseX < posXbot14 + tamBot14 &&
-    mouseY > posYbot14 && mouseY < posYbot14 + tamBot14) {
-    colSel = 3
-  }
+  // Anteojos
 
-  if (mouseX > posXbot15 && mouseX < posXbot15 + tamBot15 &&
-    mouseY > posYbot15 && mouseY < posYbot15 + tamBot15) {
-    colSel = 4
-  }
+  line(480, 75, 511, 75);
+  ellipse(480, 75, 15, 15);
+  ellipse(511, 75, 15, 15);
 
-  if (mouseX > posXbot16 && mouseX < posXbot16 + tamBot16 &&
-    mouseY > posYbot16 && mouseY < posYbot16 + tamBot16) {
-    colSel = 5
-  }
+  // Cristal
 
-  if (mouseX > posXbot17 && mouseX < posXbot17 + tamBot17 &&
-    mouseY > posYbot17 && mouseY < posYbot17 + tamBot17) {
-    colSel = 6
-  }
+  quad(535, 55, 565, 65, 575, 95, 545, 85);
+  line(565, 65, 545, 85);
 
-  if (mouseX > posXbot18 && mouseX < posXbot18 + tamBot18 &&
-    mouseY > posYbot18 && mouseY < posYbot18 + tamBot18) {
-    colSel = 7
-  }
+  // 
 
-  if (mouseX > posXbot19 && mouseX < posXbot19 + tamBot19 &&
-    mouseY > posYbot19 && mouseY < posYbot19 + tamBot19) {
-    colSel = 8
-  }
+  ellipse(615, 75, 40, 10);
+  line(595, 75, 595, 55);
+  line(635, 75, 635, 95);
 
-  if (mouseX > posXbot20 && mouseX < posXbot20 + tamBot20 &&
-    mouseY > posYbot20 && mouseY < posYbot20 + tamBot20) {
-    colSel = 9
-  }
 
   //Comando de que hace cada boton
 
   if (mouseIsPressed) {
 
+    
+  if (mouseX > posXbot1 && mouseX < posXbot1 + tamBot1 &&
+    mouseY > posYbot1 && mouseY < posYbot1 + tamBot1) {
+    herSel = 0;
+  }
+
+  if (mouseX > posXbot2 && mouseX < posXbot2 + tamBot2 &&
+    mouseY > posYbot2 && mouseY < posYbot2 + tamBot2) {
+    herSel = 1;
+  }
+
+  if (mouseX > posXbot3 && mouseX < posXbot3 + tamBot3 &&
+    mouseY > posYbot3 && mouseY < posYbot3 + tamBot3) {
+    herSel = 2;
+  }
+
+  if (mouseX > posXbot4 && mouseX < posXbot4 + tamBot4 &&
+    mouseY > posYbot4 && mouseY < posYbot4 + tamBot4) {
+    herSel = 3;
+  }
+
+  if (mouseX > posXbot5 && mouseX < posXbot5 + tamBot5 &&
+    mouseY > posYbot5 && mouseY < posYbot5 + tamBot5) {
+    herSel = 4;
+  }
+
+  if (mouseX > posXbot6 && mouseX < posXbot6 + tamBot6 &&
+    mouseY > posYbot6 && mouseY < posYbot6 + tamBot6) {
+    herSel = 5;
+  }
+
+  if (mouseX > posXbot7 && mouseX < posXbot7 + tamBot7 &&
+    mouseY > posYbot7 && mouseY < posYbot7 + tamBot7) {
+    herSel = 6;
+  }
+
+  if (mouseX > posXbot8 && mouseX < posXbot8 + tamBot8 &&
+    mouseY > posYbot8 && mouseY < posYbot8 + tamBot8) {
+    herSel = 7;
+  }
+
+  if (mouseX > posXbot9 && mouseX < posXbot9 + tamBot9 &&
+    mouseY > posYbot9 && mouseY < posYbot9 + tamBot9) {
+    herSel = 8;
+  }
+
+  if (mouseX > posXbot10 && mouseX < posXbot10 + tamBot10 &&
+    mouseY > posYbot10 && mouseY < posYbot10 + tamBot10) {
+    herSel = 9;
+  }
+
+  if (mouseX > posXbot11 && mouseX < posXbot11 + tamBot11 &&
+    mouseY > posYbot11 && mouseY < posYbot11 + tamBot11) {
+    colSel = 0;
+  }
+
+  if (mouseX > posXbot12 && mouseX < posXbot12 + tamBot12 &&
+    mouseY > posYbot12 && mouseY < posYbot12 + tamBot12) {
+    colSel = 1;
+  }
+
+  if (mouseX > posXbot13 && mouseX < posXbot13 + tamBot13 &&
+    mouseY > posYbot13 && mouseY < posYbot13 + tamBot13) {
+    colSel = 2;
+  }
+
+  if (mouseX > posXbot14 && mouseX < posXbot14 + tamBot14 &&
+    mouseY > posYbot14 && mouseY < posYbot14 + tamBot14) {
+    colSel = 3;
+  }
+
+  if (mouseX > posXbot15 && mouseX < posXbot15 + tamBot15 &&
+    mouseY > posYbot15 && mouseY < posYbot15 + tamBot15) {
+    colSel = 4;
+  }
+
+  if (mouseX > posXbot16 && mouseX < posXbot16 + tamBot16 &&
+    mouseY > posYbot16 && mouseY < posYbot16 + tamBot16) {
+    colSel = 5;
+  }
+
+  if (mouseX > posXbot17 && mouseX < posXbot17 + tamBot17 &&
+    mouseY > posYbot17 && mouseY < posYbot17 + tamBot17) {
+    colSel = 6;
+  }
+
+  if (mouseX > posXbot18 && mouseX < posXbot18 + tamBot18 &&
+    mouseY > posYbot18 && mouseY < posYbot18 + tamBot18) {
+    colSel = 7;
+  }
+
+  if (mouseX > posXbot19 && mouseX < posXbot19 + tamBot19 &&
+    mouseY > posYbot19 && mouseY < posYbot19 + tamBot19) {
+    colSel = 8;
+  }
+
+  if (mouseX > posXbot20 && mouseX < posXbot20 + tamBot20 &&
+    mouseY > posYbot20 && mouseY < posYbot20 + tamBot20) {
+    colSel = 9;
+  }
+
     if (herSel == 0) {
       ellipse(mouseX, mouseY, 50, 50);
-      triangle(mouseX - 25, mouseY, mouseX + 25, mouseY, mouseX, mouseY + 60)
+      triangle(mouseX - 25, mouseY, mouseX + 25, mouseY, mouseX, mouseY + 60);
     }
 
     if (herSel == 1) {
@@ -304,22 +368,23 @@ function draw() {
 
     if (colSel == 1) {
       fill(255, 117, 20);
-      stroke(0)
+      stroke(0);
+      
     }
 
     if (colSel == 2) {
       fill(34, 113, 179);
-      stroke(0)
+      stroke(0);
     }
 
     if (colSel == 3) {
       fill(248, 243, 53);
-      stroke(0)
+      stroke(0);
     }
 
     if (colSel == 4) {
       fill(248, 0, 0);
-      stroke(0)
+      stroke(0);
     }
 
     if (colSel == 5) {
@@ -345,6 +410,7 @@ function draw() {
     if (colSel == 9) {
       background(fond);
       
+      stroke(0);
       
       //Icono del helado para el boton
 
@@ -409,7 +475,7 @@ function draw() {
       line(635, 75, 635, 95);
 
       fill(250, 244, 227);
-      rect(20, 20, 650, 170, )
+      rect(20, 20, 650, 170)
           
        //Botones visibles
 
@@ -448,68 +514,5 @@ function draw() {
     }
   }
 
-
-
-  //Icono del helado para el boton
-
-  ellipse(75, 65, 25, 25);
-  triangle(60, 65, 90, 65, 75, 98);
-
-  //Icono de la flecha para el boton
-
-  line(115, 55, 155, 95);
-  line(115, 55, 155, 55);
-  line(115, 55, 115, 95);
-
-  //Icono reloj de arena para boton
-
-  quad(175, 55, 215, 55, 175, 95, 215, 95);
-
-  //Icono X
-
-  line(235, 55, 275, 95);
-  line(275, 55, 235, 95);
-
-  //Icono estrella
-
-  ellipse(315, 75, 15, 15);
-  triangle(310, 70, 295, 75, 310, 80);
-  triangle(310, 70, 315, 55, 321, 70);
-  triangle(321, 70, 335, 75, 321, 80);
-  triangle(321, 80, 315, 95, 310, 80);
-
-  // Sol
-
-  line(375, 75, 375, 100);
-  line(375, 75, 375, 50);
-  line(375, 75, 400, 75);
-  line(375, 75, 350, 75);
-  line(375, 75, 400, 100);
-  line(375, 75, 350, 50);
-  line(375, 75, 400, 50);
-  line(375, 75, 350, 100);
-  ellipse(375, 75, 25, 25);
-
-  // Cruz con ellipses
-
-  ellipse(435, 75, 10, 40);
-  ellipse(435, 75, 40, 10);
-
-  // Anteojos
-
-  line(480, 75, 511, 75);
-  ellipse(480, 75, 15, 15);
-  ellipse(511, 75, 15, 15);
-
-  // Cristal
-
-  quad(535, 55, 565, 65, 575, 95, 545, 85);
-  line(565, 65, 545, 85);
-
-  // 
-
-  ellipse(615, 75, 40, 10);
-  line(595, 75, 595, 55);
-  line(635, 75, 635, 95);
 
 }
